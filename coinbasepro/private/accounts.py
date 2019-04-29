6539dc2e1edde1a610038a3d4ec4f98d434850e5
David Hull <david.hull37@gmail.com>
2019-04-29 10:52:06 -0500

    Sandbox url's and account set up

--- coinbasepro/private/accounts.py
+++ coinbasepro/private/accounts.py
@@ -1,19 +1,33 @@
 import os, requests, json
-from .coinbase_auth import CoinbaseAuth
+from coinbase_auth import CoinbaseAuth
 from requests.auth import AuthBase
 from dotenv import load_dotenv
 load_dotenv()
 
-KEY = os.getenv('API_KEY')
-SECRET = os.getenv('API_SECRET')
-PASS = os.getenv('API_PASS')
+# Un-comment this for using real account
+# KEY = os.getenv('API_KEY')
+# SECRET = os.getenv('API_SECRET')
+# PASS = os.getenv('API_PASS')
+
+# Using Sandbox API for testing and development found at https://public.sandbox.pro.coinbase.com
+SAND_KEY = os.getenv('SAND_KEY')
+SAND_SECRET = os.getenv('SAND_SECRET')
+SAND_PASS = os.getenv('SAND_PASS')
 
 class Accounts:
     def __init__(self):
-        self.url = 'https://api.pro.coinbase.com'
+        # URL for actual api
+        # self.url = 'https://api.pro.coinbase.com'
 
-    def list_trading_accounts(self, auth=None):
-        accounts = requests.get(self.url + 'accounts', auth)
+        # URL for sandbox
+        self.url = 'https://api-public.sandbox.pro.coinbase.com/'
+
+    def list_trading_accounts(self, autho):
+        accounts = requests.get(self.url + 'accounts', auth=autho)
         return accounts.json()
 
 
+
+autho = CoinbaseAuth(SAND_KEY, SAND_SECRET, SAND_PASS)
+account = Accounts()
+print(account.list_trading_accounts(autho))
